be98eea48e008d05df6f756f378303924c954803
ryomaruyama <marusin618@gmail.com>
2025-12-30 12:00:13 +0900

    fontfamilyを変更

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,20 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, DotGothic16, Press_Start_2P } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
+const dotGothic16 = DotGothic16({ 
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-dot-gothic16",
+});
+const pressStart2P = Press_Start_2P({ 
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-press-start-2p",
+});
 
 export const metadata: Metadata = {
   title: "丸山涼のポートフォリオ",
@@ -18,7 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body className={inter.className}>
+      <body className={`${inter.className} ${dotGothic16.variable} ${pressStart2P.variable}`}>
         <Header />
         {children}
         <Footer ownerName="丸山涼" year={2024} />
